8145212c180e636ff71664179d1069bf26029cc5
PraNAtaP <pranatapu08@gmail.com>
2025-05-21 16:26:49 +0700

    Commit Praktikum 1

---
+++ Pertemuan13/dll/DoubleLinkedList19.java
@@ -0,0 +1,120 @@
+package Pertemuan13.dll;
+
+public class DoubleLinkedList19 {
+    Node19 head;
+    Node19 tail;
+
+    public DoubleLinkedList19() {
+        head = null;
+        tail = null;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void addFirst(Mahasiswa19 data) {
+        Node19 newNode = new Node19(data);
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+    }
+
+    public void addLast(Mahasiswa19 data) {
+        Node19 newNode = new Node19(data);
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+    }
+
+    public void insertAfter(String keyNim, Mahasiswa19 data) {
+        Node19 current = head;
+
+        while (current != null && !current.data.nim.equals(keyNim)) {
+            current = current.next;
+        }
+
+        if (current == null) {
+            System.out.println("Node dengan NIM " + keyNim + " tidak ditemukan.");
+            return;
+        }
+        Node19 newNode = new Node19(data);
+
+        if (current == tail) {
+            current.next = newNode;
+            newNode.prev = current;
+            tail = newNode;
+        } else {
+            newNode.next = current.next;
+            newNode.prev = current;
+            current.next.prev = newNode;
+            current.next = newNode;
+        }
+        System.out.println("Node berhasil disisipkan setelah NIM " + keyNim);
+    }
+
+    public void print() {
+        if (isEmpty()) {
+            System.out.println("Peringatan: Linked list masih kosong.");
+            return;
+        }
+        Node19 current = head;
+        while (current != null) {
+            current.data.tampil();
+            current = current.next;
+        }
+    }
+
+    public void removeFirst() {
+        if (isEmpty()) {
+            System.out.println("List kosong, tidak bisa dihapus.");
+            return;
+        }
+        if (head == tail) {
+            System.out.println("Data sudah berhasil dihapus. Data yang terhapus adalah : ");
+            head.data.tampil();
+            head = tail = null;
+        } else {
+            System.out.println("Data sudah berhasil dihapus. Data yang terhapus adalah : ");
+            head.data.tampil();
+            head = head.next;
+            head.prev = null;
+        }
+    }
+
+    public void removeLast() {
+        if (isEmpty()) {
+            System.out.println("List kosong, tidak bisa dihapus.");
+            return;
+        }
+        if (head == tail) {
+            System.out.println("Data sudah berhasil dihapus. Data yang terhapus adalah : ");
+            tail.data.tampil();
+            head = tail = null;
+        } else {
+            System.out.println("Data sudah berhasil dihapus. Data yang terhapus adalah : ");
+            tail.data.tampil();
+            tail = tail.prev;
+            tail.next = null;
+        }
+    }
+
+    public Node19 search(String nim) {
+        Node19 current = head;
+        while (current != null) {
+            if (current.data.nim.equals(nim)) {
+                return current;
+            }
+            current = current.next;
+        }
+        return null;
+    }
+}
