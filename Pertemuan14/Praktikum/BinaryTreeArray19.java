c1b67718a8ca5f2816fb514c20bc8f619d638545
PraNAtaP <pranatapu08@gmail.com>
2025-06-04 13:24:27 +0700

    Commit Tugas - Modifikasi No4

--- Pertemuan14/Praktikum/BinaryTreeArray19.java
+++ Pertemuan14/Praktikum/BinaryTreeArray19.java
@@ -6,11 +6,21 @@ public class BinaryTreeArray19 {
 
     public BinaryTreeArray19() {
         this.dataMahasiswa = new Mahasiswa19[10];
+        this.idxLast = -1;
     }
 
     void populateData(Mahasiswa19 dataMhs[], int idxLast) {
         this.dataMahasiswa = dataMhs;
         this.idxLast = idxLast;
+    }
+
+    public void add(Mahasiswa19 data) {
+        if (idxLast + 1 >= dataMahasiswa.length) {
+            System.out.println("Array penuh, tidak bisa menambah data.");
+            return;
+        }
+        idxLast++;
+        dataMahasiswa[idxLast] = data;
     }
 
     void traverseInOrder(int idxStart) {
@@ -20,6 +30,18 @@ public class BinaryTreeArray19 {
                 dataMahasiswa[idxStart].tampilInformasi();
                 traverseInOrder(2 * idxStart + 2);
             }
+        }
+    }
+
+    public void traversePreOrder() {
+        traversePreOrderHelper(0);
+    }
+
+    public void traversePreOrderHelper(int idx) {
+        if (idx <= idxLast && dataMahasiswa[idx] != null) {
+            dataMahasiswa[idx].tampilInformasi();
+            traversePreOrderHelper(2 * idx + 1);
+            traversePreOrderHelper(2 * idx + 2);
         }
     }
 }
